832bb9de09c915e9cc31588404a115d512a0efab
kylearora <kylearora@gmail.com>
2017-08-17 18:39:26 -0500

    Final Commit

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+const container = document.querySelector('.container')
+let player = document.querySelector('.music-player')
+let results = document.querySelector('.results')
+let submitButtom = document.querySelector('.submitButton')
+
+  const form = document.querySelector(".search-form")
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const resultsDisplay = document.querySelector(".resultsDisplay")
+    resultsDisplay.innerHTML =""// destroy all the content
+
+    const input = document.querySelector("#inputField").value;
+    console.log(input)
+
+    fetch(`https://itunes.apple.com/search?term=${input}`)
+    .then(function(data){
+      return data.json()
+    }).then(function(json){
+      console.log(json);
+
+      for (var i = 0; i < json.results.length; i++) {
+
+        const searchResults = `
+
+        <div class ="searchResultsItems">
+        <img class ="resultsImage" src="${json.results[i].artworkUrl100}" value=${json.results[i].previewUrl}>
+          <a href="https://en.wikipedia.org/wiki/${input}"><h3 class ="resultsArtist">${json.results[i].artistName}</h3></a>
+          <a href="${json.results[i].previewUrl}"><h4 class="resultsTitle" value = ${json.results[i].previewUrl}>${json.results[i].trackName}</h4></a>
+        </div>
+
+        `
+        resultsDisplay.insertAdjacentHTML("beforeEnd", searchResults)
+      }
+
+      let resultsImage = document.querySelector(".resultsImage")
+      results.addEventListener('click', function(e) {
+        if (e.target && e.target.nodeName == "IMG") {
+          player.src = e.target.getAttribute('value')
+        }
+    })
+  })
+})
